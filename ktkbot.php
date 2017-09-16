0d29cedec0aca8aa875c2e44e7261af10bbc722f
Alexander K. Burko <aleksandr_burko@mail.ru>
2017-09-17 01:23:05 +0700

    Перенос получения данных в класс телеграм бот

--- ktkbot.php
+++ ktkbot.php
@@ -8,13 +8,11 @@ require __DIR__.'/vendor/autoload.php';
 
 $log = new Logger('TelegramBot');
 $log->pushHandler(new StreamHandler(__DIR__.'/logs/info.log', Logger::INFO));
+
 $tb = new Bot();
 
-$request = file_get_contents("php://input");
+$log->info($tb->getRequest());
 
-$log->info($request);
-
-echo $request;
 
 
 
--- src/Telegram/Bot.php
+++ src/Telegram/Bot.php
@@ -8,15 +8,22 @@ class Bot
 {
     private $_config;
     private $_api_url;
+    private $_request;
 
     function __construct()
     {
         $this->_config = Yaml::parse(file_get_contents(__DIR__.'/../../config/config.yml'));
         $this->_api_url = $this->_config['api_url'] . $this->_config['bot_token'];
+        $this->_request = file_get_contents('php://input');
     }
 
     public function getApiURL()
     {
         return $this->_api_url;
+    }
+
+    public function getRequest()
+    {
+        return $this->_request;
     }
 }
