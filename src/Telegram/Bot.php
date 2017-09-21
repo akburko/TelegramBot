719377c68a7cae1f8bc01405a6506e94f29bdb98
Alexander K. Burko <aleksandr_burko@mail.ru>
2017-09-22 00:00:23 +0700

    Отправка ответа в чат телеграм

--- src/Telegram/Bot.php
+++ src/Telegram/Bot.php
@@ -3,18 +3,21 @@
 namespace Telegram;
 
 use Symfony\Component\Yaml\Yaml;
+use GuzzleHttp\Client;
 
 class Bot
 {
     private $_config;
     private $_api_url;
     private $_request;
+    private $_client;
 
     function __construct()
     {
         $this->_config = Yaml::parse(file_get_contents(__DIR__.'/../../config/config.yml'));
-        $this->_api_url = $this->_config['api_url'] . $this->_config['bot_token'];
+        $this->_api_url = $this->_config['api_url'] . $this->_config['bot_token'] . '/';
         $this->_request = json_decode(file_get_contents('php://input'));
+        $this->_client = new Client(['base_uri' => $this->_api_url]);
     }
 
     public function getApiURL()
@@ -32,8 +35,10 @@ class Bot
         $commands = explode(' ',$this->_request->message->text);
         $command = substr($commands[0],1);
         if (isset($this->_config['commands'][$command]))
-            return $this->_config['commands'][$command]['text'];
+            return $this->_client->get('sendMessage', array('json' => array('chat_id' => $this->_request->chat->id, "text" => $this->_config['commands'][$command]['text'])));
         else
             return 'The command is not exists.';
     }
+
+
 }
